356edd5df242596b9a337b0fae084eb053322a4b
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-09-24 18:18:26 +0300

    Completed Project

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+
 import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div className='app__newsletter'>
-    <div className='app__newsletter-heading'>
-      <SubHeading title ='Newsletter'/>
-      <h1 className='headtext__cormorant'>Subscribe to Our Newsletter</h1>
-      <p className='p__opensans'>Never miss latest updates</p>
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
     </div>
-    <div className='app__newsletter-input flex__center'>
-      <input type='email' placeholder='Enter your e-mail address' />
-      <button className='custom__button'>Subscribe</button>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <button type="button" className="custom__button">Subscribe</button>
     </div>
   </div>
 );
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
-import {FooterOverlay, Newsletter} from '../../components'
-import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi'
-import {images} from '../../constants'
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div className='app__footer section__padding'>
+  <div className="app__footer section__padding" id="login">
     <FooterOverlay />
     <Newsletter />
-    <div className='app__footer-links'>
-      <div className='app__footer-links_contact'>
-        <h1 className='app__footer-headtext'>Contact Us</h1>
-        <p className='p__opensans'>Nairobi, Kenya</p>
-        <p className='p__opensans'>+254 763485022</p>
-        <p className='p__opensans'>+245 719845320</p>
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">+1 212-344-1230</p>
+        <p className="p__opensans">+1 212-555-1230</p>
       </div>
-      <div className='app__footer-links_logo'>
-        <img src={images.gericht} alt='logo'/>
-        <p className='p__opensans'> The bes way to achieve something is to try</p>
-        <img src={images.spoon} alt='spoon' className='spoon__img' style={{marginTop:15}}/>
-        <div className='app__footer-links_icons'>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">&quot;The best way to accomplish something is by trying.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
           <FiFacebook />
           <FiTwitter />
           <FiInstagram />
         </div>
       </div>
-      <div className='app__footer-links_work'>
-      <h1 className='app__footer-headtext'>Working Hours</h1>
-        <p className='p__opensans'>Monday-Friday: </p>
-        <p className='p__opensans'>08:00am-12:00am</p>
-        <p className='p__opensans'>Saturday-Sunday: </p>
-        <p className='p__opensans'>07:00am-11:00pm</p>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
       </div>
     </div>
-    <div className='footer__copyright'>
-      <p className='p__opensans'>2023 Yukio. All rights  preserved</p>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2023 yukio. All Rights reserved.</p>
     </div>
+
   </div>
 );
 
